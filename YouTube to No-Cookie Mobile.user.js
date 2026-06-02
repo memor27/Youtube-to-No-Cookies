c1f292994e2ac05e294a303362b86a95f71b2055
klasputnikov <100342106+memor27@users.noreply.github.com>
2026-06-02 16:01:57 +0200

    Add userscript for YouTube no-cookie redirection
    This userscript redirects YouTube links to a no-cookie version on mobile devices.

---
+++ YouTube to No-Cookie Mobile.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         YouTube to No-Cookie Mobile
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Redirect youtube to no-cookies on mobile
+// @author       Klasputnikov
+// @match        https://www.youtube.com/watch*
+// @match        https://m.youtube.com/watch*
+// @match 		 https://youtu.be/watch*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const url = window.location.href;
+
+    const newUrl = url.replace(/youtube\.com/, 'yout-ube.com');
+
+    if (newUrl !== url) {
+        window.location.replace(newUrl);
+    }
+})();
